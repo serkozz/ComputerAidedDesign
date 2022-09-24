60ba7fe769982be54fa68de1998cb384c28d1952
Сергей Козлов <serezha-kozlov.2002@mail.ru>
2022-09-24 18:51:17 +0400

    Lab2_AlmostDone

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,28 +29,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.createTreeButton = new System.Windows.Forms.Button();
+            this.createDocumentTreeButton = new System.Windows.Forms.Button();
             this.createTreeOnDiskButton = new System.Windows.Forms.Button();
-            this.addObject = new System.Windows.Forms.Button();
+            this.addObjectButton = new System.Windows.Forms.Button();
             this.removeObjectButton = new System.Windows.Forms.Button();
-            this.treeView = new System.Windows.Forms.TreeView();
+            this.documentTreeView = new System.Windows.Forms.TreeView();
+            this.objectTreeView = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.removeDocumentButton = new System.Windows.Forms.Button();
+            this.addDocumentButton = new System.Windows.Forms.Button();
+            this.useObjectStructureCheckBox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
-            // createTreeButton
+            // createDocumentTreeButton
             // 
-            this.createTreeButton.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.createTreeButton.Location = new System.Drawing.Point(12, 12);
-            this.createTreeButton.Name = "createTreeButton";
-            this.createTreeButton.Size = new System.Drawing.Size(152, 70);
-            this.createTreeButton.TabIndex = 0;
-            this.createTreeButton.Text = "Сформировать дерево каталогов в программе";
-            this.createTreeButton.UseVisualStyleBackColor = true;
-            this.createTreeButton.Click += new System.EventHandler(this.createTreeButton_Click);
+            this.createDocumentTreeButton.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.createDocumentTreeButton.Location = new System.Drawing.Point(12, 12);
+            this.createDocumentTreeButton.Name = "createDocumentTreeButton";
+            this.createDocumentTreeButton.Size = new System.Drawing.Size(152, 91);
+            this.createDocumentTreeButton.TabIndex = 0;
+            this.createDocumentTreeButton.Text = "Сформировать дерево каталогов в программе по видам документов";
+            this.createDocumentTreeButton.UseVisualStyleBackColor = true;
+            this.createDocumentTreeButton.Click += new System.EventHandler(this.createTreeButton_Click);
             // 
             // createTreeOnDiskButton
             // 
             this.createTreeOnDiskButton.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.createTreeOnDiskButton.Location = new System.Drawing.Point(12, 88);
+            this.createTreeOnDiskButton.Location = new System.Drawing.Point(12, 107);
             this.createTreeOnDiskButton.Name = "createTreeOnDiskButton";
             this.createTreeOnDiskButton.Size = new System.Drawing.Size(152, 70);
             this.createTreeOnDiskButton.TabIndex = 1;
@@ -58,57 +64,130 @@
             this.createTreeOnDiskButton.UseVisualStyleBackColor = true;
             this.createTreeOnDiskButton.Click += new System.EventHandler(this.createTreeOnDiskButton_Click);
             // 
-            // addObject
+            // addObjectButton
             // 
-            this.addObject.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.addObject.Location = new System.Drawing.Point(12, 164);
-            this.addObject.Name = "addObject";
-            this.addObject.Size = new System.Drawing.Size(152, 70);
-            this.addObject.TabIndex = 2;
-            this.addObject.Text = "Добавить объект в программе";
-            this.addObject.UseVisualStyleBackColor = true;
-            this.addObject.Click += new System.EventHandler(this.addObject_Click);
+            this.addObjectButton.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.addObjectButton.Location = new System.Drawing.Point(12, 183);
+            this.addObjectButton.Name = "addObjectButton";
+            this.addObjectButton.Size = new System.Drawing.Size(152, 70);
+            this.addObjectButton.TabIndex = 2;
+            this.addObjectButton.Text = "Добавить объект в программе";
+            this.addObjectButton.UseVisualStyleBackColor = true;
+            this.addObjectButton.Click += new System.EventHandler(this.addObjectButton_Click);
             // 
             // removeObjectButton
             // 
             this.removeObjectButton.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.removeObjectButton.Location = new System.Drawing.Point(12, 240);
-            this.removeObjectButton.Name = "removeObject";
+            this.removeObjectButton.Location = new System.Drawing.Point(12, 259);
+            this.removeObjectButton.Name = "removeObjectButton";
             this.removeObjectButton.Size = new System.Drawing.Size(152, 70);
             this.removeObjectButton.TabIndex = 3;
             this.removeObjectButton.Text = "Удалить объект в программе";
             this.removeObjectButton.UseVisualStyleBackColor = true;
-            this.removeObjectButton.Click += new System.EventHandler(this.removeObject_Click);
+            this.removeObjectButton.Click += new System.EventHandler(this.removeObjectButton_Click);
             // 
-            // treeView
+            // documentTreeView
             // 
-            this.treeView.Location = new System.Drawing.Point(170, 12);
-            this.treeView.Name = "treeView";
-            this.treeView.Size = new System.Drawing.Size(600, 729);
-            this.treeView.TabIndex = 4;
+            this.documentTreeView.Location = new System.Drawing.Point(170, 49);
+            this.documentTreeView.Name = "documentTreeView";
+            this.documentTreeView.Size = new System.Drawing.Size(300, 692);
+            this.documentTreeView.TabIndex = 4;
+            // 
+            // objectTreeView
+            // 
+            this.objectTreeView.Location = new System.Drawing.Point(476, 49);
+            this.objectTreeView.Name = "objectTreeView";
+            this.objectTreeView.Size = new System.Drawing.Size(300, 692);
+            this.objectTreeView.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(200, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Структура документов по видам";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(499, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(258, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Структура документов по объектам";
+            // 
+            // removeDocumentButton
+            // 
+            this.removeDocumentButton.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.removeDocumentButton.Location = new System.Drawing.Point(12, 411);
+            this.removeDocumentButton.Name = "removeDocumentButton";
+            this.removeDocumentButton.Size = new System.Drawing.Size(152, 70);
+            this.removeDocumentButton.TabIndex = 9;
+            this.removeDocumentButton.Text = "Удалить вид документов в программе";
+            this.removeDocumentButton.UseVisualStyleBackColor = true;
+            this.removeDocumentButton.Click += new System.EventHandler(this.removeDocumentButton_Click);
+            // 
+            // addDocumentButton
+            // 
+            this.addDocumentButton.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.addDocumentButton.Location = new System.Drawing.Point(12, 335);
+            this.addDocumentButton.Name = "addDocumentButton";
+            this.addDocumentButton.Size = new System.Drawing.Size(152, 70);
+            this.addDocumentButton.TabIndex = 8;
+            this.addDocumentButton.Text = "Добавить вид документов в программе";
+            this.addDocumentButton.UseVisualStyleBackColor = true;
+            this.addDocumentButton.Click += new System.EventHandler(this.addDocumentButton_Click);
+            // 
+            // useObjectStructureCheckBox
+            // 
+            this.useObjectStructureCheckBox.AutoSize = true;
+            this.useObjectStructureCheckBox.Location = new System.Drawing.Point(12, 498);
+            this.useObjectStructureCheckBox.Name = "useObjectStructureCheckBox";
+            this.useObjectStructureCheckBox.Size = new System.Drawing.Size(141, 64);
+            this.useObjectStructureCheckBox.TabIndex = 10;
+            this.useObjectStructureCheckBox.Text = "Использование\r\nструктуры\r\nпо объектам\r\n";
+            this.useObjectStructureCheckBox.UseVisualStyleBackColor = true;
+            this.useObjectStructureCheckBox.CheckedChanged += new System.EventHandler(this.useObjectStructureCheckBox_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(782, 753);
-            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.useObjectStructureCheckBox);
+            this.Controls.Add(this.removeDocumentButton);
+            this.Controls.Add(this.addDocumentButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.objectTreeView);
+            this.Controls.Add(this.documentTreeView);
             this.Controls.Add(this.removeObjectButton);
-            this.Controls.Add(this.addObject);
+            this.Controls.Add(this.addObjectButton);
             this.Controls.Add(this.createTreeOnDiskButton);
-            this.Controls.Add(this.createTreeButton);
+            this.Controls.Add(this.createDocumentTreeButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
         // Made by Sergey Kozlov 4-IAIT-3
         #endregion
 
-        private Button createTreeButton;
+        private Button createDocumentTreeButton;
         private Button createTreeOnDiskButton;
-        private Button addObject;
+        private Button addObjectButton;
         private Button removeObjectButton;
-        private TreeView treeView;
+        private TreeView documentTreeView;
+        private TreeView objectTreeView;
+        private Label label1;
+        private Label label2;
+        private Button removeDocumentButton;
+        private Button addDocumentButton;
+        private CheckBox useObjectStructureCheckBox;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,8 +5,11 @@ namespace SAPR1
     public partial class Form1 : Form
     {
         public string TestDir { get; private set; } = @"C:\TEST\";
-        public int RootNodesCount { get; private set; } = 1;
+        public int DocumentRootNodesCount { get; private set; } = 1;
+        public int DocumentObjectNodesCount { get; private set; } = 1;
+        public int ObjectRootNodesCount { get; private set; } = 1;
         public int ObjectNodesCount { get; private set; } = 1;
+        public List<(string, List<string>, Color)> DocumentNodeTupleList { get; private set; } = new List<(string, List<string>, Color)>();
 
         public Form1()
         {
@@ -44,46 +47,102 @@ namespace SAPR1
 
         private void createTreeButton_Click(object sender, EventArgs e)
         {
-            TreeCreate();
+            if (useObjectStructureCheckBox.Checked)
+            {
+                FillDocumentTuple();
+                TreeCreate(TreeViewType.ObjectTreeView);
+            }
+            else if (!useObjectStructureCheckBox.Checked)
+                TreeCreate(TreeViewType.DocumentTreeView);
             createTreeOnDiskButton.Enabled = true;
         }
 
-        private void TreeCreate()
+        private void FillDocumentTuple()
         {
-            TreeClear();
-            AddRootNode("ИРД", new List<string>() { "ЗУ", "ГПЗУ", "РС", "ТУ", "ПрДекл"}, Color.Green);
-            AddRootNode("ИИ", new List<string>() { "ИГдИ", "ИГлИ", "ИЭИ"}, Color.Blue);
-            AddRootNode("ПД", new List<string>() { "ПЗ", "СПОЗУ", "АР", "КР"}, Color.Red);
-            AddRootNode("РД", new List<string>() { "ГП", "КЖ"}, Color.Orange);
-            treeView.ExpandAll();
+            DocumentNodeTupleList.Add(("ИРД", new List<string>() { "ЗУ", "ГПЗУ", "РС", "ТУ", "ПрДекл"}, Color.Green));
+            DocumentNodeTupleList.Add(("ИИ", new List<string>() { "ИГдИ", "ИГлИ", "ИЭИ"}, Color.Blue));
+            DocumentNodeTupleList.Add(("ПД", new List<string>() { "ПЗ", "СПОЗУ", "АР", "КР"}, Color.Red));
+            DocumentNodeTupleList.Add(("РД", new List<string>() { "ГП", "КЖ"}, Color.Orange));
         }
 
-        private void TreeClear()
+        private void TreeCreate(TreeViewType treeViewType)
         {
-            treeView.Nodes.Clear();
-            RootNodesCount = 1; ObjectNodesCount = 1;
+            
+            if (treeViewType == TreeViewType.DocumentTreeView)
+            {
+                TreeClear(TreeViewType.DocumentTreeView);
+                AddDocumentRootNode("ИРД", new List<string>() { "ЗУ", "ГПЗУ", "РС", "ТУ", "ПрДекл"}, Color.Green);
+                AddDocumentRootNode("ИИ", new List<string>() { "ИГдИ", "ИГлИ", "ИЭИ"}, Color.Blue);
+                AddDocumentRootNode("ПД", new List<string>() { "ПЗ", "СПОЗУ", "АР", "КР"}, Color.Red);
+                AddDocumentRootNode("РД", new List<string>() { "ГП", "КЖ"}, Color.Orange);
+                documentTreeView.ExpandAll();
+            }
+            else if (treeViewType == TreeViewType.ObjectTreeView)
+            {
+                TreeClear(TreeViewType.ObjectTreeView);
+                AddObjectRootNode($"Объект {ObjectRootNodesCount}", DocumentNodeTupleList);
+                objectTreeView.ExpandAll();
+            }
         }
 
-        private void AddRootNode(string text, List<string> innerNodesText, Color innerNodesTextColor)
+        private void TreeClear(TreeViewType treeViewType)
         {
-            TreeNode rootNode = new TreeNode($"{RootNodesCount.ToString()}. {text}");
-            TreeNode innerNodesCollection = new TreeNode($"{RootNodesCount.ToString()}.{ObjectNodesCount.ToString()} Объект {ObjectNodesCount.ToString()}");
+            if (treeViewType == TreeViewType.DocumentTreeView)
+            {
+                documentTreeView.Nodes.Clear();
+                DocumentRootNodesCount = 1; DocumentObjectNodesCount = 1;
+            }
+            else if (treeViewType == TreeViewType.ObjectTreeView)
+            {
+                objectTreeView.Nodes.Clear();
+                ObjectRootNodesCount = 1; ObjectNodesCount = 1;
+            }
+        }
+
+        private void AddDocumentRootNode(string text, List<string> innerNodesText, Color innerNodesTextColor)
+        {
+            TreeNode rootNode = new TreeNode($"{DocumentRootNodesCount.ToString()}. {text}");
+            TreeNode innerNodesCollection = new TreeNode($"{DocumentRootNodesCount.ToString()}.{DocumentObjectNodesCount.ToString()} Объект {DocumentObjectNodesCount.ToString()}");
 
             int index = 1; // Номер пункта внутренней ноды 
 
             foreach (var str in innerNodesText)
             {
-                innerNodesCollection.Nodes.Add($"{RootNodesCount.ToString()}.{ObjectNodesCount.ToString()}.{index} {str}");
+                innerNodesCollection.Nodes.Add($"{DocumentRootNodesCount.ToString()}.{DocumentObjectNodesCount.ToString()}.{index} {str}");
                 index++;
             }
             ColorNode(innerNodesCollection, innerNodesTextColor);
 
             rootNode.Nodes.Add(innerNodesCollection);
-            treeView.Nodes.Add(rootNode);
-            RootNodesCount++;
+            documentTreeView.Nodes.Add(rootNode);
+            DocumentRootNodesCount++;
         }
 
-        private void UpdateClonedNodeObjectText(TreeNode node, int rootNodeIndex, int objectIndex)
+        private void AddObjectRootNode(string text, List<(string documentNodeText, List<string> innerNodesText, Color innerNodesTextColor)> documentNodeTupleList)
+        {
+            TreeNode rootNode = new TreeNode($"{ObjectRootNodesCount.ToString()}. {text}");
+
+            for (int i = 0; i <= documentNodeTupleList.Count - 1; i++)
+            {
+                TreeNode innerNodesCollection = new TreeNode($"{ObjectRootNodesCount.ToString()}.{ObjectNodesCount.ToString()} {documentNodeTupleList[i].documentNodeText}");
+
+                int innerIndex = 1; // Номер пункта внутренней ноды 
+
+                foreach (var str in documentNodeTupleList[i].innerNodesText)
+                {
+                    innerNodesCollection.Nodes.Add($"{ObjectRootNodesCount.ToString()}.{ObjectNodesCount.ToString()}.{innerIndex} {str}");
+                    innerIndex++;
+                }
+                ColorNode(innerNodesCollection, documentNodeTupleList[i].innerNodesTextColor);
+                rootNode.Nodes.Add(innerNodesCollection);
+                ObjectNodesCount++;
+            }
+
+            objectTreeView.Nodes.Add(rootNode);
+            ObjectRootNodesCount++;
+        }
+
+        private void UpdateClonedDocumentNodeObjectText(TreeNode node, int rootNodeIndex, int objectIndex)
         {
             int nodeIndex = 1;
             foreach (TreeNode innerNode in node.Nodes)
@@ -93,7 +152,7 @@ namespace SAPR1
                 nodeIndex++;
             }
         }
-        
+
         private void ColorNode(TreeNode treeNode, Color color)
         {
             foreach (TreeNode node in treeNode.Nodes)
@@ -104,56 +163,92 @@ namespace SAPR1
 
         private void createTreeOnDiskButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Операция успешно выполнена!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
             if(Directory.Exists(TestDir))
                 Directory.Delete(TestDir, true);
 
-            CallRecursive(treeView);
+            if (useObjectStructureCheckBox.Checked)
+                CallRecursive(objectTreeView);
+            else
+                CallRecursive(documentTreeView);
+
+            MessageBox.Show("Операция успешно выполнена!", "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        private void addObject_Click(object sender, EventArgs e)
+        private void addObjectButton_Click(object sender, EventArgs e)
         {
             int rootNodeIndex = 0;
             int updatedRootNodeIndex = 0;
 
-            ObjectNodesCount = treeView.Nodes[0].Nodes.Count;
-            ObjectNodesCount++;
+            DocumentObjectNodesCount = documentTreeView.Nodes[0].Nodes.Count;
+            DocumentObjectNodesCount++;
 
-            foreach (TreeNode node in treeView.Nodes)
+            foreach (TreeNode node in documentTreeView.Nodes)
             {
-                TreeNode? clonedNode = treeView.Nodes[rootNodeIndex].Nodes[treeView.Nodes[rootNodeIndex].Nodes.Count - 1].Clone() as TreeNode;
+                TreeNode? clonedNode = documentTreeView.Nodes[rootNodeIndex].Nodes[documentTreeView.Nodes[rootNodeIndex].Nodes.Count - 1].Clone() as TreeNode;
                 
                 if (clonedNode != null)
                 {
-                    clonedNode.Text = $"{(rootNodeIndex + 1).ToString()}.{ObjectNodesCount.ToString()} Объект {ObjectNodesCount.ToString()}";
+                    clonedNode.Text = $"{(rootNodeIndex + 1).ToString()}.{DocumentObjectNodesCount.ToString()} Объект {DocumentObjectNodesCount.ToString()}";
                     updatedRootNodeIndex = rootNodeIndex + 1;
-                    UpdateClonedNodeObjectText(clonedNode, updatedRootNodeIndex, ObjectNodesCount);
+                    UpdateClonedDocumentNodeObjectText(clonedNode, updatedRootNodeIndex, DocumentObjectNodesCount);
                 }
                 else
                     return;
 
-                treeView.Nodes[rootNodeIndex].Nodes.Add(clonedNode);
+                documentTreeView.Nodes[rootNodeIndex].Nodes.Add(clonedNode);
                 rootNodeIndex++;
             }
             removeObjectButton.Enabled = true;
-            treeView.ExpandAll();
+            documentTreeView.ExpandAll();
         }
 
-        private void removeObject_Click(object sender, EventArgs e)
+        private void removeObjectButton_Click(object sender, EventArgs e)
         {
-            if (ObjectNodesCount > 1)
+            if (DocumentObjectNodesCount > 1)
             {
-                if (ObjectNodesCount == 2) removeObjectButton.Enabled = false;
-                int rootNodeIndex = treeView.Nodes.Count - 1;
-                foreach (TreeNode node in treeView.Nodes)
+                if (DocumentObjectNodesCount == 2) removeObjectButton.Enabled = false;
+                int rootNodeIndex = documentTreeView.Nodes.Count - 1;
+                foreach (TreeNode node in documentTreeView.Nodes)
                 {
-                    int nodesCount = treeView.Nodes[node.Level].Nodes.Count;
-                    treeView.Nodes[rootNodeIndex].Nodes.RemoveAt(nodesCount-1);
+                    int nodesCount = documentTreeView.Nodes[node.Level].Nodes.Count;
+                    documentTreeView.Nodes[rootNodeIndex].Nodes.RemoveAt(nodesCount-1);
                     rootNodeIndex--;
                 }
-                ObjectNodesCount--;
+                DocumentObjectNodesCount--;
             }
+        }
+
+        private void addDocumentButton_Click(object sender, EventArgs e)
+        {
+            DocumentNodeTupleList.Add(("Новый документ", new List<string>() { "Неопределено", "Неизвестно"}, Color.Brown));
+            TreeCreate(TreeViewType.ObjectTreeView);
+        }
+
+        private void removeDocumentButton_Click(object sender, EventArgs e)
+        {
+            DocumentNodeTupleList.RemoveAt(DocumentNodeTupleList.Count - 1);
+            TreeCreate(TreeViewType.ObjectTreeView);
+        }
+
+        private void useObjectStructureCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox? cb = sender as CheckBox;
+
+            if (cb != null && cb.Checked)
+            {
+                addObjectButton.Enabled = true;
+                removeObjectButton.Enabled = true;
+                addDocumentButton.Enabled = true;
+                removeDocumentButton.Enabled = true;
+            }
+            if (cb != null && !cb.Checked)
+            {
+                addObjectButton.Enabled = true;
+                removeObjectButton.Enabled = true;
+                addDocumentButton.Enabled = true;
+                removeDocumentButton.Enabled = true;
+            }
+
         }
     }
 }
